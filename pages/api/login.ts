70037d676e2b5b1d0213d4660728485a2282a41f
Antônio Apolinário Gonzaga Neto <120532863+Apolinar1o@users.noreply.github.com>
2023-12-27 17:41:44 -0300

    .

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -11,15 +11,16 @@ const endpointLogin = async (
     req : NextApiRequest,
     res : NextApiResponse<RespostaPadraoMsg | LoginResposta>
 ) => {
-
     const {MINHA_CHAVE_JWT} = process.env;
     if(!MINHA_CHAVE_JWT){
         return res.status(500).json({erro : 'ENV Jwt nao informada'});
     }
 
     if(req.method === 'POST'){
+
         const {login, senha} = req.body;
         const usuariosEncontrados = await UsuarioModel.find({email : login, senha : md5(senha)});
+
         if(usuariosEncontrados && usuariosEncontrados.length > 0){
             const usuarioEncotrado = usuariosEncontrados[0];
 
