6657d0a2c65ff46b357f5af51435986cfdd2e2c4
Antônio Apolinário Gonzaga Neto <120532863+Apolinar1o@users.noreply.github.com>
2023-12-30 11:56:23 -0300

    vercel

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -19,12 +19,17 @@ const endpointLogin = async (
     }
 
     if(req.method === 'POST'){
+        console.log("2222222222211111111111111111111111")
 
         const {login, senha} = req.body;
         const usuariosEncontrados = await UsuarioModel.find({email : login, senha : md5(senha)});
+        console.log("222222222221333333333333333333")
 
         if(usuariosEncontrados && usuariosEncontrados.length > 0){
+            console.log("usuario encontrado")
+
             const usuarioEncotrado = usuariosEncontrados[0];
+         
 
             const token = jwt.sign({_id : usuarioEncotrado._id}, MINHA_CHAVE_JWT);
             return res.status(200).json({
