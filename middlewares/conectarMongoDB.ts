32b4b1481fc3fd375530c8ff519eecf7ca5a9d5c
Antônio Apolinário Gonzaga Neto <120532863+Apolinar1o@users.noreply.github.com>
2023-12-30 14:40:57 -0300

    .

--- middlewares/conectarMongoDB.ts
+++ middlewares/conectarMongoDB.ts
@@ -27,8 +27,7 @@ export const conectarMongoDB = (handler : NextApiHandler) =>
         mongoose.connection.on('error', error => console.log(`Ocorreu erro ao conectar no banco: ${error}`));
         console.log("33334555555555555555555555555555")
         console.log("tentativa de conexao")
-        console.log(DB_CONEXAO_STRING + " olá mundo")
-        await mongoose.connect("mongodb+srv://aapolinarioneto:hyslla21@cluster0.3amaest.mongodb.net");
+        mongoose.connect("mongodb+srv://aapolinarioneto:hyslla21@cluster0.3amaest.mongodb.net");
     
         console.log("333366666666666666666666666666")
     } catch (e) {
